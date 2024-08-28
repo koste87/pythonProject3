0e5eedad0f810d323d6c6f2b99bf1f37ae7b0345
koste87 <kostel8877@gmail.com>
2024-08-28 09:34:20 +0300

    add 1

--- kliker.py
+++ kliker.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 
-def no():
+def Yes():
     messagebox.showinfo(' ', 'Спасибо! Ваш голос учтён!')
     quit()
 
@@ -15,13 +15,13 @@ root.title('Опрос')
 root.resizable(width=False, height=False)
 root['bg'] = 'white'
 
-label = Label(root, text='Боря Пидор?', font='Arial 20 bold', bg='white')
+label = Label(root, text='Федюшкин Пидор?', font='Arial 20 bold', bg='white')
 label.pack()
 
 btnYes = Button(root, text='Да', font='Arial 20 bold')
 btnYes.place(x=170, y=100)
 
-btnNo = Button(root, text='Нет', font='Arial 20 bold', command=no)
+btnNo = Button(root, text='Нет', font='Arial 20 bold', command=NO)
 btnNo.place(x=350, y=100)
 
 btnNo.bind('<Enter>', motionMouse)
